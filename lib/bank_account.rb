bc9bfd4b64e33d918c750bc211118a42301608c7
HolsDuckett <hf.duckett@gmail.com>
2021-03-08 15:41:01 +0000

    deposit method added, amount only, date to be added

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,18 @@
 class BankAccount
 
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
   def print_statement
     "date || credit || debit || balance"
   end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,12 +1,27 @@
 require 'bank_account'
+require 'Date'
 
 describe 'Bank Account' do
 
   let (:account) {BankAccount.new}
   let (:statement_header) { "date || credit || debit || balance" }
+  let (:todays_date) {Date.today}
 
-  it 'should print out a customers bank statement' do
-    expect(account.print_statement).to eq(statement_header)
+  describe '#print_statament' do
+
+    it 'should print out a customers bank statement' do
+      expect(account.print_statement).to eq(statement_header)
+    end
+
+  end
+
+  describe '#deposit' do
+
+    it "should increase balance by desposit amount" do
+      expect{ account.deposit(100) }.to change{ account.balance }.by(100)
+    end
+    
+
   end
 
 
